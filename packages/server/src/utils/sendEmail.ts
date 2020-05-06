f4b1037aabc5fae2e160f5d83d1adab311315086
dkim22 <ehddnr1186@gmail.com>
2020-05-06 18:51:57 +0900

    Change mail lib to nodemailer

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,23 +1,62 @@
-import * as SparkPost from "sparkpost";
+// import * as SparkPost from "sparkpost";
 
-const client = new SparkPost(process.env.SPARKPOST_API_KEY);
+// const client = new SparkPost(process.env.SPARKPOST_API_KEY);
+
+// export const sendEmail = async (recipients: string, url: string) => {
+//   const response = await client.transmissions.send({
+//     options: {
+//       sandbox: true,
+//     },
+//     content: {
+//       from: "testing@sparkpostbox.com",
+//       subject: "Confirm Email",
+//       html: `<html>
+//         <body>
+//         <p>Testing SparkPost - the world's most awesomest email service!</p>
+//         <a href="${url}">confirm email</a>
+//         </body>
+//         </html>`,
+//     },
+//     recipients: [{ address: recipients }],
+//   });
+//   console.log(response);
+// };
+
+
+// Use at least Nodemailer v4.1.0
+import * as nodemailer from "nodemailer";
 
 export const sendEmail = async (recipients: string, url: string) => {
-  const response = await client.transmissions.send({
-    options: {
-      sandbox: true,
-    },
-    content: {
-      from: "testing@sparkpostbox.com",
-      subject: "Confirm Email",
-      html: `<html>
-        <body>
-        <p>Testing SparkPost - the world's most awesomest email service!</p>
-        <a href="${url}">confirm email</a>
-        </body>
-        </html>`,
-    },
-    recipients: [{ address: recipients }],
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: 'corine41@ethereal.email',
+      pass: '9rHN8Jn5RdBywfPYGf'
+    }
   });
-  console.log(response);
+
+  // Message object
+  const message = {
+    from: 'Sender Name <sender@example.com>',
+    to: `Recipient <${recipients}>`,
+    subject: 'Nodemailer is unicode friendly ✔',
+    text: 'Hello to myself!',
+    html: `<html>
+      <body>
+      <p>Testing SparkPost - the world's most awesomest email service!</p>
+      <a href="${url}">confirm email</a>
+      </body>
+      </html>`,
+  };
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log('Error occurred. ' + err.message);
+    }
+
+    console.log('Message sent: %s', info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  });
 };
